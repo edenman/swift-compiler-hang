9ebf75e47d31a4a0a3fdb0293a4c0995b36c86cc
Eric Denman <edenman@gmail.com>
2017-04-21 09:57:35 -0700

    Initial Commit

---
+++ swift-compiler-hang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-compiler-hang
+//
+//  Created by Eric Denman on 4/21/17.
+//  Copyright © 2017 Eric Denman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
